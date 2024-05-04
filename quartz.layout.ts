e6c3e52b3df409a3e1e380e48e9972865a009099
leiyu3 <s444814187@gmail.com>
2024-05-04 09:13:26 -0400

    removed OG Image

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,55 +3,55 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
-    },
-  }),
+    head: Component.Head(),
+    header: [],
+    footer: Component.Footer({
+        links: {
+            GitHub: "https://github.com/jackyzha0/quartz",
+            "Discord Community": "https://discord.gg/cRFFHYye7t",
+        },
+    }),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.TableOfContents()),
-  ],
-  right: [
-    Component.Graph({
-        localGraph: {
-            depth: 2,
-            showTags: false,
-        },
-        globalGraph: {
-            showTags: false,
-        }
-    }),
-    Component.DesktopOnly(),
-    Component.Backlinks(),
-  ],
+    beforeBody: [
+        Component.Breadcrumbs(),
+        Component.ArticleTitle(),
+        Component.ContentMeta(),
+        Component.TagList(),
+    ],
+    left: [
+        Component.PageTitle(),
+        Component.MobileOnly(Component.Spacer()),
+        Component.Search(),
+        Component.Darkmode(),
+        Component.DesktopOnly(Component.TableOfContents()),
+    ],
+    right: [
+        Component.Graph({
+            localGraph: {
+                depth: 2,
+                showTags: false,
+            },
+            globalGraph: {
+                showTags: false,
+            }
+        }),
+        Component.DesktopOnly(),
+        Component.Backlinks(),
+    ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [],
+    beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+    left: [
+        Component.PageTitle(),
+        Component.MobileOnly(Component.Spacer()),
+        Component.Search(),
+        Component.Darkmode(),
+        Component.DesktopOnly(Component.Explorer()),
+    ],
+    right: [],
 }
